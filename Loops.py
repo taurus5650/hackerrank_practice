663a61f9a3efbc48fc544005a7291253aaf067d7
taurus5650 <taurus_5650@hotmail.com>
2022-05-07 21:44:32 +0800

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    
+    for i in range (n) :
+    # n : means if input 5 then execute 5 times
+        print (i*i)
+        # i*i : square number (1^2 = 1 ; 2^2 =4 ; 3^2 = 9 ; 4^2 = 16)
+        # https://i1.kknews.cc/s8cXVr0EeMdC1ui8WnxGtSl3OwyePdOSBmPvUQY/0.jpg
