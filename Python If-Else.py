9fe1a898e2165592dddac8ace2299d16298785bc
taurus5650 <taurus_5650@hotmail.com>
2022-05-07 20:49:28 +0800

    Create Python If-Else.py

---
+++ Python If-Else.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def task () :
+    n = int(input().strip())
+    
+    if n %2 != 0 : 
+        print ("Weird")
+    elif n > 20 :
+        print ("Not Weird")
+                
+        
+
+
+
+if __name__ == '__main__':
+    task ()
